88ea305bab678ab99f35d2549d23e9c65832fc97
Pat <patrick.s.santucci@gmail.com>
2019-05-20 13:48:42 +0100

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find_student(params[:id])
   end
 
-  private
+  def edit
+    binding.pry
+    @student = Student.find_student(params[:id])
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  end
+
+  def
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def self.find_student(id)
+    Student.find(id)
+  end
 end
